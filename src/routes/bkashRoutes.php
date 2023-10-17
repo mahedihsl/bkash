52c1a07d2f888c445e0f99d58d3d3e7fa7914eb8
mahedi <mahedihsl2022@gmail.com>
2023-10-17 12:58:52 +0600

    clen

--- src/routes/bkashRoutes.php
+++ src/routes/bkashRoutes.php
@@ -2,23 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use Mahedi250\Bkash\App\Controllers\CheckoutUrlController;
-use Mahedi250\Bkash\Facade\CheckoutUrl;
+
+
+
+
+
 
 Route::group(['middleware' => ['web']], function () {
-
-
-    Route::get("/bkash",function(){
-        // session(['test_key' => 'test_value']);
-        $response = CheckoutUrl::Create(100);
-
-            return redirect($response['bkashURL']);
-
-    });
 
     Route::get("bkash/callback",[CheckoutUrlController::class,'Callback']);
     Route::get("bkash/failed",[CheckoutUrlController::class,'Failed'])->name('bkash.payment.fail');
     Route::get("bkash/success",[CheckoutUrlController::class,'Success'])->name('bkash.payment.success');
-    // Your routes here
+
 });
 
 
