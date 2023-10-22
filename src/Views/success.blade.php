5f97822bef2ceb02a3bbe0073fc0083da5aaa6c5
mahedi <mahedihsl2022@gmail.com>
2023-10-22 18:29:44 +0600

    cleansups

--- src/Views/success.blade.php
+++ src/Views/success.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DONE</title>
+    <title>Successful</title>
     <style>
         body {
             display: flex;
--- src/routes/bkashRoutes.php
+++ src/routes/bkashRoutes.php
@@ -10,7 +10,7 @@ use Mahedi250\Bkash\Facade\CheckoutUrl;
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::get("/bkash",function(){ return redirect(CheckoutUrl::Create(102,['payerReference'=>"01877722345"])['bkashURL']);});
+    //Route::get("/bkash",function(){ return redirect(CheckoutUrl::Create(102,['payerReference'=>"01877722345",'intent'=>'authorization'])['bkashURL']);});
     Route::get("bkash/callback",[CheckoutUrlController::class,'Callback']);
     Route::get("bkash/failed",[CheckoutUrlController::class,'Failed'])->name('bkash.payment.fail');
     Route::get("bkash/success",[CheckoutUrlController::class,'Success'])->name('bkash.payment.success');
